b382c68c3adf2d845742691aa72f884713d60aeb
ifxvillegas1 <159083602+ifxvillegas1@users.noreply.github.com>
2024-02-05 17:38:13 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesbtn = document.querySelector('#yesbtn');
+
+yesbtn.addEventListener('click',function (){
+    alert('Te espero el 14')
+});
+
+const nobtn = document.querySelector('#nobtn');
+
+nobtn.addEventListener('click', function (){
+    alert('Esta bien 😿')
+});
